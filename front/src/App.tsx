c334654517d984607ed0235f931739a06dfae1e5
hguerni <hava3@hotmail.fr>
2022-03-28 18:01:29 +0200

    original compose fixed

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,4 @@
-import './index.scss';
-import React from 'react';
-import logo from './logo.svg';
+import logo from './logoT.png';
 import './App.css';
 
 function App() {
@@ -8,20 +6,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Transcendance </h1>
+        <button type="button" onClick={window.alert}>CONNECTE TOI !</button>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './Router/Router';
+import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
-/**
- * @malatini
- * Fichier "racine" pour l'app frontend
- * Pour l'instant j'utilise useContext et useMemo comme "Cookies"
- */
 ReactDOM.render(
   <BrowserRouter>
-    <CookiesProvider>
         <App />
-    </CookiesProvider>
   </BrowserRouter>,
   document.getElementById("root")
 );
