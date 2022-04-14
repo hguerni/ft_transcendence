e6b038d64e04cfed76cbaeefdc1d4a4cc194f14e
Elihaza <elias.larbi@gmail.com>
2022-04-15 01:17:23 +0200

    added start transparent button

--- rayanetest/front/src/App.tsx
+++ rayanetest/front/src/App.tsx
@@ -1,16 +1,20 @@
 import logo from './ICON.svg';
 import logo_entier from './TRANSCENDENCE.svg';
+import logo_start from './START.svg';
 import './App.css';
 
 function App() {
+  function handleClick() {
+    alert('clicked');
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <img src={logo_entier} className="App-logo2" alt="logo_entier" />
-
-        <button type="button" onClick={window.alert}>CONNECTE TOI !</button>
-        
+        <img src={logo_start} className="App-logo3" alt="logo_start" />
+        <button type="button" onClick={handleClick}>hahahah</button>
       </header>
     </div>
   );
