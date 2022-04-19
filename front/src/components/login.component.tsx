1f563b5e9481444f5134448ee6d49bbf2b83bbf0
hguerni <hava3@hotmail.fr>
2022-04-19 17:31:48 +0200

    merged

--- front/src/components/login.component.tsx
+++ front/src/components/login.component.tsx
@@ -1,19 +1,24 @@
 
 import { Component } from "react";
-import logo from '../logoT.png';
+import logo from '../images/ICON.svg';
+import logo_entier from '../images/TRANSCENDENCE.svg';
+import logo_start from '../images/START.svg';
 import '../App.css';
 
 
 class Login extends Component {
   render() {
       return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h1>Transcendance </h1>
-            <a href={'http://localhost:3030/auth/login'}> <button type="button">CONNECTE TOI !</button></a>
-          </header>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo_entier} className="trs_full" alt="logo_entier" /> 
+        <div className="start_button">
+          <img src={logo_start} className="start_image" alt="logo_start" />
+          <a href={'http://localhost:3030/auth/login'}><button type="button" id="strt_btn"></button></a>
         </div>
+      </header>
+    </div>
     )}
 }
 
