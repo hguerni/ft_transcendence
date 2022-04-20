ba31d777d35f4a4da6b5a609305d44beb5cadd4b
Pierre SEMSARI NEJAD <psemsari@e3r11p10.clusters.42paris.fr>
2022-04-20 16:20:00 +0200

    chat getaway afterinit test

--- rayanetest/back/src/websocket/chat.getaway.ts
+++ rayanetest/back/src/websocket/chat.getaway.ts
@@ -4,31 +4,67 @@ import {
 	SubscribeMessage,
 	MessageBody,
 	ConnectedSocket,
-	OnGatewayConnection
+	OnGatewayConnection,
+	OnGatewayInit,
+	OnGatewayDisconnect
   } from '@nestjs/websockets';
 
   import { Server, Socket } from 'socket.io'
 
-  @WebSocketGateway()
-  export class ChatGateway implements OnGatewayConnection{
-	  @WebSocketServer() io: Server;
+  @WebSocketGateway({namespace: 'chat'})
+  export class ChatGateway implements OnGatewayConnection, OnGatewayInit, OnGatewayDisconnect{
+	@WebSocketServer() io: Server;
 
-	  Clients = new Map<string, Socket>();
+	Clients = new Map<string, Socket>();
 
-	  @SubscribeMessage('connection')
-	  handleConnection(
-		  @ConnectedSocket() client: Socket
-	  )
-	  {
-		  this.Clients[client.handshake.auth.name] = client;
-		  console.log(client.handshake.auth.name);
-	  }
+	afterInit() {
+	this.io.use((socket, next) => {
+		const sessionID = socket.handshake.auth.sessionID;
+		if (sessionID) {
+		  const session = sessionStore.findSession(sessionID);
+		  if (session) {
+			socket.sessionID = sessionID;
+			socket.userID = session.userID;
+			socket.username = session.username;
+			return next();
+		  }
+		}
+		const username = socket.handshake.auth.username;
+		if (!username) {
+		  return next(new Error("invalid username"));
+		}
+		socket.sessionID = randomId();
+		socket.userID = randomId();
+		socket.username = username;
+		next();
+	});
+	}
 
-	  @SubscribeMessage('private-message')
-	  handleEvent(
-		  @MessageBody() {name, message}: {name: string, message: string}
-	  ): void
-	  {
-		  this.Clients[name].emit('private-message', {name, message});
-	  }
+	@SubscribeMessage('connection')
+	handleConnection(
+		@ConnectedSocket() client: Socket
+	)
+	{
+		// persist session
+		sessionStore.saveSession(client.sessionID, {
+			userID: client.userID,
+			username: client.username,
+			connected: true,
+		});
+	}
+
+	@SubscribeMessage('private-message')
+	handleEvent(
+		@MessageBody() {name, message}: {name: string, message: string}
+	): void
+	{
+		this.Clients[name].emit('private-message', {name, message});
+	}
+
+	@SubscribeMessage('disconnect')
+	handleDisconnect(
+		@MessageBody() {name, message}: {name: string, message: string}
+	): void
+	{
+	}
   }
