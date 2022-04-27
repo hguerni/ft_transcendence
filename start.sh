78135cab320617324499296284249dde0f9992f3
ellarbi <ellarbi@student.42.fr>
2022-04-27 17:10:47 +0200

    fixed script removing node modules

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
-cd front && rm -rf package-lock.json node_modules && npm i
-cd ../back && rm -rf package-lock.json node_modules && npm i
+cd front && npm i
+cd ../back && npm i
 docker system prune -a && docker compose up --build
