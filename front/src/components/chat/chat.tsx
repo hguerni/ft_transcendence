ba01bdf689a899efd953fa32de3b3e235dae2237
ellarbi <ellarbi@student.42.fr>
2022-05-03 15:54:38 +0200

    mini private chat box degueu

--- front/src/components/chat/chat.tsx
+++ front/src/components/chat/chat.tsx
@@ -1,4 +1,7 @@
+import React, { Component } from "react";
+import './chat.css';
 import { RestaurantRounded } from "@mui/icons-material";
+import ClearIcon from '@mui/icons-material/Clear';
 import { Button } from "@mui/material";
 
 function ButtonCreateCanal(){
@@ -22,10 +25,48 @@ function Chat() {
 
     return (
         <>
-            
-         <ButtonCreateCanal/>
-            
+            <ButtonCreateCanal/>
+            <DirectMessages />
         </>
+    );
+}
+
+function DirectMessages() {
+    return (
+        <div className="direct-messages">
+            <div className="direct-messages-header">
+                <div className="direct-messages-header-title">
+                    <span>Direct Messages</span>
+                    <ClearIcon id="close-icon"/>
+                </div>
+                
+            </div>
+            <div className="direct-messages-list">
+                <div className="direct-messages-list-item">
+                    <div className="direct-messages-list-item-content">
+                        <div className="direct-messages-list-item-content-name">
+                            <span>John Doe</span>
+                        </div>
+                        <div className="direct-messages-list-item-content-message">
+                            <span>Hello, how are you?</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="direct-messages-list-item">
+                    <div className="direct-messages-list-item-content">
+                        <div className="direct-messages-list-item-content-name">
+                            <span>John Doe</span>
+                        </div>
+                        <div className="direct-messages-list-item-content-message">
+                            <span>Hello, how are you?</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="direct-messages-write">
+                    <input type="text" placeholder="Write message" />
+                </div>
+        </div>
     );
 }
 
