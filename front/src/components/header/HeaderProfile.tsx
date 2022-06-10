d1355c9897f45d30b549ff85877a7b065e5dd46a
lnezonde <34407290+lnezonde@users.noreply.github.com>
2022-06-10 15:50:53 +0200

    added logout button on navbar

--- front/src/components/header/HeaderProfile.tsx
+++
@@ -1,9 +0,0 @@
-import account from "../../images/account-circle.svg";
-
-export default function HeaderProfile () {
-  return (
-    <a href="/profile">
-      <img className="accountImg" src={account}/>
-    </a>
-  );
-}
